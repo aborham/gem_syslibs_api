ade2b3ceed2d8f1c731926ebc9d736c7bddcb562
Ahmed Borham <saladin2002eg@gmail.com>
2016-08-12 18:51:17 +0200

    Update ReadMe file

--- spec/controllers/api/v1/packages_controller_spec.rb
+++ spec/controllers/api/v1/packages_controller_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::PackagesController, type: :controller do
 
+  describe "GET #index" do
+    it "returns welcome message" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST #search" do
+    pending "add some examples to  #{__FILE__}"
+    # it "returns packages data and libraries" do
+    #   data = {"packages":["nokogiri"],"os":"Linux"}
+    #   post :search
+    #   expect(response).to have_http_status(:success)
+    # end
+  end
+
 end
