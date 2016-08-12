937d3b5c6c4befcc270782a5d215e37625b53d80
Ahmed Borham <saladin2002eg@gmail.com>
2016-08-12 17:21:12 +0200

    remove api subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ require 'api_constraints'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: /.+/ }, path:  '/' do
+  namespace :api, defaults: { format: :json }  do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-
       resources :packages,  except: [:new, :edit]
       post '/search', to: 'packages#search'
     end
